3004b0ac6d0da5dd9b3f4103b6a2a9d769cc9670
kristina_svn <svnkrist@gmail.com>
2014-07-08 23:56:41 +0400

    main file

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Тишкаа on 08.07.2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
