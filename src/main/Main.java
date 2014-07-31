562f407fb1d51687059cc0b195c82cc03ad99536
davidluckystar <david.lucky.star@gmail.com>
2014-07-31 11:13:36 +0400

    fibbonaci

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,28 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!!!");
+        int length = 0;
+        int[] fib = fibbonacci(length);
+        for (int element : fib) {
+            System.out.println(element);
+        }
+
+    }
+
+    static int[] fibbonacci(int n) {
+        int[] fib = new int[n];
+        if (n == 0) {
+            return fib;
+        }
+        if (n == 1) {
+            fib[0] = 0;
+            return fib;
+        }
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i=2; i<fib.length; i++) {
+            fib[i] = fib[i-2] + fib[i-1];
+        }
+        return fib;
     }
 }
